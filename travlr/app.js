a80b308987fb0c95bb41aecf0f587a1ac5441480
Connor Bailey <26304831+TechnoEquinox@users.noreply.github.com>
2025-11-16 17:08:05 -0500

    json
    Replaced static HTML with JSON templates

--- travlr/app.js
+++ travlr/app.js
@@ -12,7 +12,7 @@ var handlebars = require('hbs');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app_server', 'views'));
 
 // register handlebars partials
 handlebars.registerPartials(__dirname + '/app_server/views/partials');
@@ -27,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/travel', travelRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- travlr/app_server/routes/travel.js
+++ travlr/app_server/routes/travel.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 var controller = require('../controllers/travel');
 
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf-8'));
+
 /* GET travel page */
 router.get('/', controller.travel);
 
