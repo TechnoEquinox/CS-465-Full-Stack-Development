a52bfcb744bd1f6551aeb2347d97260d24239a27
Connor Bailey <26304831+TechnoEquinox@users.noreply.github.com>
2025-11-16 18:29:48 -0500

    fix
    - Fixed an issue where the static HTML was still being rendered instead of the JSON template

--- travlr/app_server/controllers/travel.js
+++ travlr/app_server/controllers/travel.js
@@ -1,6 +1,13 @@
+var fs = require('fs');
+var path = require('path')
+// var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf-8'));
+
+var trips = JSON.parse(fs.readFileSync(path.join(__dirname, '..', '..', 'data', 'trips.json'), 'utf-8'));
+
 /* GET travel view */
 const travel = (req, res) => {
-    res.render('travel', { title: 'Travlr Gataways'});
+    console.log('Trips being rendered:', trips.map(t => t.description));
+    res.render('travel', { title: 'Travlr Gataways', trips});
 };
 
 module.exports = {
--- travlr/app_server/routes/travel.js
+++ travlr/app_server/routes/travel.js
@@ -2,9 +2,6 @@ var express = require('express');
 var router = express.Router();
 var controller = require('../controllers/travel');
 
-var fs = require('fs');
-var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf-8'));
-
 /* GET travel page */
 router.get('/', controller.travel);
 
